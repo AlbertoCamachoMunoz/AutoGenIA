ab7a93de3f4c51886fd416c24b231640a9acb33a
alberto.camacho <alberto.camacho@grupoerik.com>
2025-06-17 08:38:57 +0200

    eliminado agente sin uso

--- infrastructure/agents/price_analyzer/dtos/price_analyzer_request_dto.py
+++
@@ -1,7 +0,0 @@
-# infrastructure/agents/price_analyzer/dtos/price_analyzer_request_dto.py
-
-from dataclasses import dataclass
-
-@dataclass
-class PriceAnalyzerRequestDTO:
-    pages: list  # Lista de diccionarios con campos 'url' y 'content'
